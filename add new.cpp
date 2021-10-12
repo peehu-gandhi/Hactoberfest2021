0b97cbce4388109389a68902842bb21415910e8a
MuskanTaya23 <92319160+MuskanTaya23@users.noreply.github.com>
2021-10-12 22:38:55 +0530

    Create add new.cpp
    updated program to add two numbers using C++ .

---
+++ add new.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int num1,num2;
+  cout<<"Enter two integer numbers : "<<endl;
+  cin>>num1>>num2;
+  cout<<"Sum of "<<num1 <<" and "<<num2 <<" is : "<<num1+num2;
+}
