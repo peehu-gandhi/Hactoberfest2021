596c10aa44648935cbb08f0d06f93fa30b4a0a2a
MANISH SINGH TOMAR <72705246+MANISH-SINGH-TOMAR@users.noreply.github.com>
2021-10-25 17:03:03 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-
+//main fuction
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers;
+    int first_Number, second_Number, sumOfTwo_Numbers;
     
     cout << "Enter two integers: ";
-    cin >> firstNumber >> secondNumber;
+    cin >> first_Number >> second_Number;
 
     // sum of two numbers in stored in variable sumOfTwoNumbers
-    sumOfTwoNumbers = firstNumber + secondNumber;
+    sumOfTwo_Numbers = first_Number + second_Number;
 
-    // Prints sum 
-    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+    // Prints sum of two Numbers 
+    cout << first_Number << " + " <<  second_Number << " = " << sumOfTwo_Numbers;     
 
     return 0;
 }
