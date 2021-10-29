7c68e0402e59a54f7f94f71edea09452cb8e3093
Fardeen Khan <69037256+Fard433n@users.noreply.github.com>
2021-10-29 20:59:23 +0530

    updated hangman.py
    please merge and consider this

--- HANGMAN_GAME.py
+++ HANGMAN_GAME.py
@@ -1,87 +1,147 @@
+#                                 Welcome to DataFlair Hangman Game:
+
+
 import random
-import termcolor
-from termcolor import colored
-print(colored("*******************************","green"))
-print(colored("*******************************","green"))
-print(colored("************HANGMAN************",'blue'))
-print(colored("*******************************","green"))
-print(colored("*******************************","green"))
-name=input(colored("ENTER THE NAME TO PLAY HANGMAN: ",'blue'))
-print(colored("Hi {} welcome to HANGMAN".format(name),'red'))
-print(colored("Enter 1 for play easy leval HANGMAN","yellow"))
-print(colored("Enter 2 for play hard leval HANGMAN",'yellow'))
-choice=input(colored('Enter the choice:','blue'))
-if choice!='1' and choice!='2':
-    print("Wrong input")
-    exit(0)
-main_list=['MOVIE','FRUIT','PROGRAMMING_LANGUAGE']
-random.shuffle(main_list)
-if main_list[0]=='FRUIT':
-    fruit = ['APPLE', 'MANGO', 'GUAVA', 'LITCHI', 'BANANA', 'ORANGE', 'COCONUT', 'BLACKPLUM','JACKFRUIT','PINEAPPLE','BERRY' ]
-    random.shuffle(fruit)
-    answer = fruit[1]
-    compiler_answer = list(answer)
-    print(colored("Guess the name of fruit:", "green"))
-elif main_list[0]=='MOVIE':
-    movie = ['RACE3', 'BAGHI2', 'RAID', 'PARI', 'PARMANU', 'PADMAN', 'PADMAVAT']
-    random.shuffle(movie)
-    answer = movie[1]
-    compiler_answer = list(answer)
-    print(colored("Guess the name of movie(relise 2018):", "magenta"))
+import time
 
-elif main_list[0]=='PROGRAMMING_LANGUAGE':
-    programming_language = ['ANDROID', 'C++', 'JAVA', 'PYTHON', 'SHIFT', 'JAVASCRIPT', 'PHP', 'HTML', 'KOTLIN', 'JSON','AJAX','JQUERY']
-    random.shuffle(programming_language)
-    answer = programming_language[1]
-    compiler_answer = list(answer)
-    print(colored("Guess the name of programming_language:", "blue"))
-
-display=[]
-display.extend(compiler_answer)
-ex_it=1
-count=0
-length=len(compiler_answer)
-if choice=='1':
-    for i in range(len(display)):
-        display[i]=" - "
-    q = []
-    for i in range(len(compiler_answer)):
-        q.append(i)
-    random.shuffle(q)
-    a = q[0]
-    guess=compiler_answer[a]
-    guess = guess.upper()
-    for i in range(len(compiler_answer)):
-        if compiler_answer[i] == guess:
-            display[i] = guess
-            length=length-1
-    print(colored("".join(display),"blue"))
-    print('\n')
-elif choice=='2':
-    for i in range(len(display)):
-        display[i]=" - "
-    print(colored(''.join(display),'blue'))
-    print("\n")
+# Initial Steps to invite in the game:
+print("\nWelcome to Hangman game by DataFlair\n")
+name = input("Enter your name: ")
+print("Hello " + name + "! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
 
 
-while count<length:
-    if ex_it<=12:
-        print("Chances left:", 13- ex_it)
-        ex_it+=1
-        guess=input(colored("Guess a letter: ",'yellow'))
-        guess=guess.upper()
-        for i in range(len(compiler_answer)):
-            if compiler_answer[i]==guess:
-                display[i]=guess
-                count+=1
-        print(colored("".join(display),"blue"))
-        print("\n")
+# The parameters we require to execute the game:
+def main():
+    global count
+    global display
+    global word
+    global already_guessed
+    global length
+    global play_game
+    words_to_guess = ["january","border","image","film","promise","kids","lungs","doll","rhyme","damage"
+                   ,"plants"]
+    word = random.choice(words_to_guess)
+    length = len(word)
+    count = 0
+    display = '_' * length
+    already_guessed = []
+    play_game = ""
+
+# A loop to re-execute the game when the first round ends:
+
+def play_loop():
+    global play_game
+    play_game = input("Do You want to play again? y = yes, n = no \n")
+    while play_game not in ["y", "n","Y","N"]:
+        play_game = input("Do You want to play again? y = yes, n = no \n")
+    if play_game == "y":
+        main()
+    elif play_game == "n":
+        print("Thanks For Playing! We expect you back again!")
+        exit()
+
+# Initializing all the conditions required for the game:
+def hangman():
+    global count
+    global display
+    global word
+    global already_guessed
+    global play_game
+    limit = 5
+    guess = input("This is the Hangman Word: " + display + " Enter your guess: \n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 or len(guess.strip()) >= 2 or guess <= "9":
+        print("Invalid Input, Try a letter\n")
+        hangman()
+
+
+    elif guess in word:
+        already_guessed.extend([guess])
+        index = word.find(guess)
+        word = word[:index] + "_" + word[index + 1:]
+        display = display[:index] + guess + display[index + 1:]
+        print(display + "\n")
+
+    elif guess in already_guessed:
+        print("Try another letter.\n")
+
     else:
-        print(colored("Hi {} you loose the game".format(name),'red'))
-        print(colored("Play next time !","red"))
-        print(colored("The answer was: {}".format(answer),'green'))
-        exit(0)
-print(colored("Hi {} you guess the word sucessfully".format(name),'green'))
-print(colored("You won the match",'green'))
+        count += 1
+
+        if count == 1:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+
+        elif count == 2:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+
+        elif count == 3:
+           time.sleep(1)
+           print("   _____ \n"
+                 "  |     | \n"
+                 "  |     |\n"
+                 "  |     | \n"
+                 "  |      \n"
+                 "  |      \n"
+                 "  |      \n"
+                 "__|__\n")
+           print("Wrong guess. " + str(limit - count) + " guesses remaining\n")
+
+        elif count == 4:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |      \n"
+                  "  |      \n"
+                  "__|__\n")
+            print("Wrong guess. " + str(limit - count) + " last guess remaining\n")
+
+        elif count == 5:
+            time.sleep(1)
+            print("   _____ \n"
+                  "  |     | \n"
+                  "  |     |\n"
+                  "  |     | \n"
+                  "  |     O \n"
+                  "  |    /|\ \n"
+                  "  |    / \ \n"
+                  "__|__\n")
+            print("Wrong guess. You are hanged!!!\n")
+            print("The word was:",already_guessed,word)
+            play_loop()
+
+    if word == '_' * length:
+        print("Congrats! You have guessed the word correctly!")
+        play_loop()
+
+    elif count != limit:
+        hangman()
 
 
+main()
+
+
+hangman()
